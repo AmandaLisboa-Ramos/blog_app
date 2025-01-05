785730ec013836ab583d68fde0eabcad1fc0905f
AmandaLisboa-Ramos <amandalisboa.am@gmail.com>
2025-01-05 20:53:21 -0300

    heroku erro 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
-  resources :post_tags
+  mount Sidekiq::Web => "/sidekiq"
+  
   devise_for :users
   devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
   end
-
-  resources :comments
+  
   resources :posts do
     resources :comments, only: [:create]
   end
+  resources :comments
+  resources :post_tags
   
   root 'posts#index'
 
